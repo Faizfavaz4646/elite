45984ca8dd6e9d7335846387e61eda147739c3f7
Faizfavaz4646 <faizfavaz7@gmail.com>
2025-05-16 14:43:05 +0530

    udated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import Home from './Components/Pages/Home';
-import Cart from './Components/Pages/Cart/Cart';
-import Login from './Components/Pages/Login/Login';
-import Wishlist from './Components/Pages/Wishlist';
-import SignUp from './Components/Pages/Signup/SignUp';
-import ProductDetails from './Components/ProductDetails';
-import ProductListing from './Components/Productlist/ProductListing';
-import Orders from './Components/Orders/Oders';
-import Payment from './Components/Payment/Payment';
+import Home from './components/Pages/Home';
+import Cart from './components/Pages/Cart/Cart';
+import Login from './components/Pages/Login/Login';
+import Wishlist from './components/Pages/Wishlist';
+import SignUp from './components/Pages/Signup/SignUp';
+import ProductDetails from './components/ProductDetails';
+import ProductListing from './components/Productlist/ProductListing';
+import Orders from './components/Orders/Oders';
+import Payment from './components/Payment/Payment';
 import { ToastContainer } from 'react-toastify';
-import Footer from './Components/Footer';
+import Footer from './components/Footer';
 import AdminDashboard from './Admins/AdminDashboard';
 import AdminProductList from './Admins/AdminProductList'; 
 import PrivateRoute from './Admins/PrivateRoute';
 import AddProduct from './Admins/AddProduct';
-import EditProfile from './Components/Pages/EditProfile/EditProfile';
+import EditProfile from './components/Pages/EditProfile/EditProfile';
 import EditProduct from './Admins/EditProduct';
 import AdminManageOrders from './Admins/AdminManageOrders';
 import AdminUserManagement from './Admins/AdminUserManagement';
 import AdminSideBar from './Admins/AdminSideBar';
-import SearchResults from './Components/SearchResults';
-import NavBar from "../src/Components/NavBar"
+import SearchResults from './components/SearchResults';
+import NavBar from "./components/NavBar"
 
 function App() {
   const location = useLocation();
