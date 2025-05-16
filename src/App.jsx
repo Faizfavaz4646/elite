cdf3f05fbadb3db503974c46742d86aabee796b8
Faizfavaz4646 <faizfavaz7@gmail.com>
2025-05-16 14:28:44 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import AdminManageOrders from './Admins/AdminManageOrders';
 import AdminUserManagement from './Admins/AdminUserManagement';
 import AdminSideBar from './Admins/AdminSideBar';
 import SearchResults from './Components/SearchResults';
-import NavBar from './Components/NavBar';
+import NavBar from "./Components/NavBar.jsx"
 
 function App() {
   const location = useLocation();
